bae98d8cdf5b6153bf048bef337e29bf3cee5714
developerbmw <6075681+developerbmw@users.noreply.github.com>
2021-09-21 20:32:42 +1200

    dockerize and update to laravel 8

---
+++ src/app/Models/User.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Models;
+
+use Auth;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function friends()
+    {
+        return $this->belongsToMany(self::class, 'friends', 'user_id', 'friend_id');
+    }
+
+    public function outgoingFriendRequests()
+    {
+        return $this->hasMany(FriendRequest::class, 'from_id');
+    }
+
+    public function incomingFriendRequests()
+    {
+        return $this->hasMany(FriendRequest::class, 'to_id');
+    }
+
+    public function transactionsFrom()
+    {
+        return $this->hasMany(Transaction::class, 'from_id');
+    }
+
+    public function transactionsTo()
+    {
+        return $this->hasMany(Transaction::class, 'to_id');
+    }
+
+    public function obligationsFrom()
+    {
+        return $this->hasMany(Obligation::class, 'from_id');
+    }
+
+    public function obligationsTo()
+    {
+        return $this->hasMany(Obligation::class, 'to_id');
+    }
+
+    public function hasObligationToCurrentUser()
+    {
+        $obligation = $this->obligationsFrom()->where('to_id', Auth::user()->id)->first();
+
+        return $obligation != null && $obligation->amount > 0.0;
+    }
+
+    public function hasObligationFromCurrentUser()
+    {
+        $obligation = $this->obligationsTo()->where('from_id', Auth::user()->id)->first();
+
+        return $obligation != null && $obligation->amount > 0.0;
+    }
+
+    public function getObligationToCurrentUser()
+    {
+        return $this->obligationsFrom()->where('to_id', Auth::user()->id)->first();
+    }
+
+    public function getObligationFromCurrentUser()
+    {
+        return $this->obligationsTo()->where('from_id', Auth::user()->id)->first();
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\FriendRequestsController;
+use App\Http\Controllers\FriendsController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionsController;
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function() {
+    Route::get('/', function() {
+        return redirect()->route('transactions.index');
+    });
+
+    Route::resource('friends', FriendsController::class)->only(['index', 'destroy']);
+    Route::resource('friend-requests', FriendRequestsController::class)->except(['show', 'edit']);
+    Route::resource('transactions', TransactionsController::class)->only(['index', 'create', 'store']);
+
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
+});
