fa2d78ddf341defc12315159d129cd499f17cbc6
developerbmw <6075681+developerbmw@users.noreply.github.com>
2021-09-24 10:38:55 +1200

    fix problem with routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,14 +11,12 @@ Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function() {
-    Route::get('/', function() {
-        return redirect()->route('transactions.index');
-    });
-
     Route::resource('friends', FriendsController::class)->only(['index', 'destroy']);
     Route::resource('friend-requests', FriendRequestsController::class)->except(['show', 'edit']);
     Route::resource('transactions', TransactionsController::class)->only(['index', 'create', 'store']);
 
     Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::redirect('/', route('transactions.index'));
 });
